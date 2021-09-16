c22e6048b482cb7ba1144f4f890e382eb1330586
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-09-16 11:13:46 +0300

    Added RecyclerView to the MemberListFragment, started getting things ready for Room database

---
+++ app/src/main/java/com/example/membersofparliamentapp/model/Member.kt
@@ -0,0 +1,21 @@
+package com.example.membersofparliamentapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "member_table")
+data class Member (
+    @PrimaryKey
+    val personNumber: Int,
+    val seatNumber: Int = 0,
+    val last: String,
+    val first: String,
+    val party: String,
+    val minister: Boolean = false,
+    val picture: String = "",
+    val twitter: String = "",
+    val bornYear: Int = 0,
+    val constituency: String = ""
+)
